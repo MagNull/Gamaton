e9720dad5e034ab98b7e8c8c5bd18a0024628199
Polina <polyabikka@gmail.com>
2023-05-07 01:40:55 +0500

    teyyetety

--- Assets/Scripts/City.cs
+++ Assets/Scripts/City.cs
@@ -1,21 +1,24 @@
 using System;
+using DefaultNamespace.Enemies;
 using UnityEngine;
 
 namespace DefaultNamespace
 {
     public class City : MonoBehaviour
     {
-        private int _health;
+        private int _health = 100;
         
         public int Health => _health;
         public event Action OnExplosion;
+        public event Action OnDamaged;
         
         
         public void TakeDamage(int damage)
         {
+            Debug.Log("Damage");
             if (damage > _health)
             {
-                Exlposion();
+                Collapse();
             }
             else
             {
@@ -23,9 +26,25 @@ namespace DefaultNamespace
             }
         }
 
-        private void Exlposion()
+        private void Collapse()
         {
+            OnExplosion += () => Destroy(gameObject);
             OnExplosion?.Invoke();
+        }
+
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.gameObject.TryGetComponent(out Diver diver))
+            {
+                TakeDamage(diver.Damage);
+                Destroy(diver.gameObject);
+            }
+            else if (col.gameObject.TryGetComponent(out Bomb bomb))
+            {
+                Debug.Log("ffff");
+                TakeDamage(bomb.Damage);
+                Destroy(bomb.gameObject);
+            }
         }
     }
 }
