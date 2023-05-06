098b906956131e9db57f21ae0bbcf03df13a761d
MagNull <pathfo@yandex.ru>
2023-05-06 16:59:33 +0500

    Da

---
+++ Assets/Scripts/Input/InputBinder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class InputBinder : MonoBehaviour
+    {
+        private Input _input;
+
+        private void Awake()
+        {
+            _input = new Input();
+        }
+
+        private void OnEnable()
+        {
+            _input.Enable();
+        }
+
+        private void OnDisable()
+        {
+            _input.Enable();
+        }
+
+        public void BindLMBCLick(Action action)
+        {
+            _input.Player.Attack.performed += _ => action.Invoke();
+        }
+
+        public void BindRMBCLick(Action action)
+        {
+            _input.Player.Ultimate.performed += _ => action.Invoke();
+        }
+    }
+}
